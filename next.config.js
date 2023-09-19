10370a5d3bd50bbb27b57881dc9abaf94c6d7e63
Ahmedmhameed <ahkhateeb02@gmail.com>
2023-09-19 12:05:59 +0300

    Update: nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
+/** @type {import('next').NextConfig} */
+const CompressionWebpackPlugin = require("compression-webpack-plugin")
+
 const nextConfig = {
+  swcMinify: true,
+  compress: true,
+  trailingSlash: true,
   images: {unoptimized: true, domains: ["ui-avatars.com"]},
-  reactStrictMode: true,
-  swcMinify: true
+  typescript: {ignoreBuildErrors: true},
+  eslint: {ignoreDuringBuilds: true},
+  webpack: (config) => {
+    config.plugins.push(
+      new CompressionWebpackPlugin({
+        test: /\.js$|\.css$|\.html$/
+      })
+    )
+
+    return config
+  }
 }
 
 module.exports = nextConfig
