dbac9921ec3105451d82e648b8e5a791e1d88859
Ahmedmhameed <ahkhateeb02@gmail.com>
2023-09-19 12:09:32 +0300

    update: IconSidebar

--- components/icon-sidebar/index.tsx
+++ components/icon-sidebar/index.tsx
@@ -1,34 +1,42 @@
 import React from "react"
 import {MdLocalOffer} from "react-icons/md"
 import {HiClipboardDocumentList} from "react-icons/hi2"
-
+import {Badge} from "@nextui-org/react"
 import MyButton from "../uis/button"
+import {useCurrentBookingOrder} from "../../stores/current-booking-order"
 
 const IconSidebar = function IconSidebar({
   setShowOffersSidebar,
   setShowBookingsSidebar,
-  isBookingNow
+  isBookingNow,
+  offersNum
 }: {
   setShowOffersSidebar: React.Dispatch<React.SetStateAction<boolean>>
   setShowBookingsSidebar: React.Dispatch<React.SetStateAction<boolean>>
   isBookingNow: boolean
+  offersNum: number
 }) {
+  const {userBookings} = useCurrentBookingOrder()
   return (
     <div className="relative">
       <div className="fixed main-hight p-2 flex flex-col z-10 bg-white/20 dark:bg-black/20 gap-3">
-        {isBookingNow ? (
-          <div>
-            <MyButton
-              size="navIcon"
-              color="white"
-              radius="sm"
-              isIconOnly
-              className="shadow-sm"
-              onClick={() => setShowOffersSidebar(true)}>
+        <div>
+          <MyButton
+            size="navIcon"
+            color="white"
+            radius="sm"
+            isIconOnly
+            className="shadow-sm"
+            onClick={() => setShowOffersSidebar(true)}>
+            <Badge
+              color={isBookingNow ? "danger" : "default"}
+              content={offersNum}
+              shape="circle"
+              disableOutline>
               <MdLocalOffer className="h-5 w-5 text-[#B9B9B9]" />
-            </MyButton>
-          </div>
-        ) : null}
+            </Badge>
+          </MyButton>
+        </div>
         <div>
           <MyButton
             size="navIcon"
@@ -37,7 +45,13 @@ const IconSidebar = function IconSidebar({
             isIconOnly
             className="shadow-sm"
             onClick={() => setShowBookingsSidebar(true)}>
-            <HiClipboardDocumentList className="h-5 w-5 text-[#B9B9B9]" />
+            <Badge
+              color="danger"
+              content={userBookings?.length}
+              shape="circle"
+              disableOutline>
+              <HiClipboardDocumentList className="h-5 w-5 text-[#B9B9B9]" />
+            </Badge>
           </MyButton>
         </div>
       </div>
