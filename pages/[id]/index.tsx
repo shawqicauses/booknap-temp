bafbcbb577c93813b1540ad28451bfd37039a19a
Ahmedmhameed <ahkhateeb02@gmail.com>
2023-09-19 12:13:29 +0300

    update: change the Booking page Structure

--- pages/[id]/index.tsx
+++ pages/[id]/index.tsx
@@ -1,18 +1,95 @@
 import {NextPage} from "next"
+import {useState, useEffect} from "react"
+import {useRouter} from "next/router"
 import HotelPageContent from "../../components/bookings/currant-booking"
 import Footer from "../../components/layout/footer"
 import Sidebar from "../../components/uis/sidebar"
 import Booking from "../../components/bookings/currant-booking/booking"
 import Protected from "../../components/uis/protected"
+import About from "../../components/bookings/currant-booking/about"
+import RoomsDetails from "../../components/bookings/currant-booking/rooms-details"
+import LoadingDiv from "../../components/uis/loading"
+import {useAuth} from "../../stores/auth"
+import client from "../../helpers/client"
+import Shop from "../../components/bookings/currant-booking/shop"
+
+const tabs = [<Booking />, <RoomsDetails />, <Shop />, <About />]
+
+export interface Datum {
+  id: number
+  name: string
+  slug: string
+  shopping_store_id: null
+  description: string
+  image: null
+  created_at: string
+  updated_at: string
+  deleted_at: null
+  hotel_id: number
+}
+export interface Test {
+  message: string
+  data: Datum[]
+}
 
 const BookingData: NextPage = function BookingData() {
+  const router = useRouter()
+  const {tab, shopTab} = router.query
+  const [categories, setCategories] = useState<Datum[]>()
+  const {token} = useAuth()
+  const id = Number(router.query.id)
+  useEffect(() => {
+    if (router.isReady && token) {
+      client(`shopping/categories?hotel_id=${id}`)?.then((res: Test) => {
+        setCategories(res.data)
+      })
+    }
+  }, [id, router, token])
+  if (!shopTab && router.isReady && categories?.length) {
+    router.push(`${id}/?tab=2&shopTab=${categories[0].id}`)
+  }
+
   return (
     <Protected>
       <div className="flex mb-10">
         <Sidebar />
         <div className="my-container">
-          <HotelPageContent />
-          <Booking />
+          <HotelPageContent>
+            {tab === "2" ? (
+              <div className="mt-5">
+                <div className="flex border-b-2 border-b-gray-200">
+                  {categories?.map(({name, id: tabId}) => (
+                    <div
+                      key={tabId}
+                      className={`cursor-pointer px-5 py-1 ${
+                        Number(shopTab) === tabId
+                          ? "border-b-2 border-b-blue-700"
+                          : ""
+                      }`}
+                      onClick={() =>
+                        router.push(`${id}/?tab=2&shopTab=${tabId}`)
+                      }
+                      aria-hidden="true">
+                      {name}
+                    </div>
+                  ))}
+                  <div
+                    className={`cursor-pointer px-5 py-1 ${
+                      Number(shopTab) === -1
+                        ? "border-b-2 border-b-blue-700"
+                        : ""
+                    }`}
+                    onClick={() => {
+                      router.push(`${id}/?tab=2&shopTab=${-1}`)
+                    }}
+                    aria-hidden="true">
+                    Favorites
+                  </div>
+                </div>
+              </div>
+            ) : null}
+          </HotelPageContent>
+          {router.isReady ? tabs[Number(tab) || 0] : <LoadingDiv />}
         </div>
       </div>
       <Footer />
--- pages/[id]/shop/[itemId].tsx
+++ pages/[id]/shop/[itemId].tsx
@@ -1,14 +1,29 @@
-import {useState} from "react"
+import {useState, useEffect} from "react"
 import {NextPage} from "next"
+import {useRouter} from "next/router"
 import Sidebar from "../../../components/uis/sidebar"
 import HotelPageContent from "../../../components/bookings/currant-booking"
 import Footer from "../../../components/layout/footer"
 import ItemPage from "../../../components/bookings/currant-booking/show-item"
 import Protected from "../../../components/uis/protected"
+import {useAuth} from "../../../stores/auth"
+import {Datum, Test} from "../index"
+import client from "../../../helpers/client"
 
 const Booking: NextPage = function Booking() {
+  const router = useRouter()
   const [activeTab, setActiveTab] = useState(1)
-
+  const [categories, setCategories] = useState<Datum[]>()
+  const {token} = useAuth()
+  const id = Number(router.query.id)
+  useEffect(() => {
+    if (router.isReady && token) {
+      client(`shopping/categories?hotel_id=${id}`)?.then((res: Test) => {
+        setCategories(res.data)
+        setActiveTab(res?.data[0]?.id)
+      })
+    }
+  }, [id, router, token])
   const handleTabClick = (tabNumber: number) => {
     setActiveTab(tabNumber)
   }
@@ -20,29 +35,24 @@ const Booking: NextPage = function Booking() {
           <HotelPageContent>
             <div className="mt-5">
               <div className="flex border-b-2 border-b-gray-200">
+                {categories?.map(({name, id: tabId}) => (
+                  <div
+                    key={tabId}
+                    className={`cursor-pointer px-5 py-1 ${
+                      activeTab === tabId ? "border-b-2 border-b-blue-700" : ""
+                    }`}
+                    onClick={() => handleTabClick(tabId)}
+                    aria-hidden="true">
+                    {name}
+                  </div>
+                ))}
                 <div
-                  className={`cursor-pointer px-4 py-2 ${
-                    activeTab === 1 ? "border-b-2 border-b-blue-700" : ""
+                  className={`cursor-pointer px-5 py-1 ${
+                    activeTab === -1 ? "border-b-2 border-b-blue-700" : ""
                   }`}
-                  onClick={() => handleTabClick(1)}
+                  onClick={() => handleTabClick(-1)}
                   aria-hidden="true">
-                  Restaurant
-                </div>
-                <div
-                  className={`cursor-pointer px-4 py-2 ${
-                    activeTab === 2 ? "border-b-2 border-b-blue-700" : ""
-                  }`}
-                  onClick={() => handleTabClick(2)}
-                  aria-hidden="true">
-                  Clothes
-                </div>
-                <div
-                  className={`cursor-pointer px-4 py-2 ${
-                    activeTab === 3 ? "border-b-2 border-b-blue-700" : ""
-                  }`}
-                  onClick={() => handleTabClick(3)}
-                  aria-hidden="true">
-                  Favorite
+                  Favorites
                 </div>
               </div>
             </div>
