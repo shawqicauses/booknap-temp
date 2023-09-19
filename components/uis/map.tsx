c8776dec05b6ad2368f3aea4e38df7300e701b40
Ahmedmhameed <ahkhateeb02@gmail.com>
2023-09-19 12:11:06 +0300

    update: google Map

--- components/uis/map.tsx
+++ components/uis/map.tsx
@@ -1,5 +1,6 @@
 import {GoogleMap, OverlayView, OverlayViewF} from "@react-google-maps/api"
-import React from "react"
+import React, {Dispatch, SetStateAction} from "react"
+import {getGeocode, getDetails} from "use-places-autocomplete"
 import {FaHotel} from "react-icons/fa"
 import {IHotel} from "../../types"
 
@@ -10,16 +11,18 @@ interface IPosition {
 
 const MyGoogleMap = function MyGoogleMap({
   pos,
+  setPos,
   userPos,
-  handleClick,
   hotels,
-  MyZoom
+  MyZoom,
+  setDestination
 }: {
   pos: IPosition
+  setPos: Dispatch<SetStateAction<IPosition | undefined>>
   userPos: IPosition
-  handleClick: () => void
   hotels: Array<IHotel>
   MyZoom: number
+  setDestination: Dispatch<SetStateAction<string>>
 }) {
   return (
     <GoogleMap
@@ -27,13 +30,47 @@ const MyGoogleMap = function MyGoogleMap({
       center={pos}
       mapContainerClassName="w-full h-full"
       options={{disableDefaultUI: true, maxZoom: 18, minZoom: 13}}
-      onClick={() => {
-        handleClick()
+      onClick={(e) => {
+        setPos({
+          lat: e.latLng?.lat() ?? pos.lat,
+          lng: e.latLng?.lng() ?? pos.lng
+        })
+        getGeocode({
+          location: {
+            lat: e.latLng?.lat() ?? pos.lat,
+            lng: e.latLng?.lng() ?? pos.lng
+          }
+        })
+          .then((value) => {
+            return value[0].place_id
+          })
+          .then((res) => {
+            getDetails({placeId: res}).then(
+              (data: string | google.maps.places.PlaceResult) => {
+                if (typeof data !== "string") {
+                  if (data?.name) {
+                    setDestination(`${data?.name}`)
+                  } else if (data?.formatted_address) {
+                    setDestination(`${data?.formatted_address}`)
+                  } else if (typeof data === "object") {
+                    if (data?.address_components) {
+                      setDestination(
+                        `${data?.address_components
+                          .map((addres) => addres?.long_name)
+                          .join(" ")}`
+                      )
+                    }
+                  }
+                }
+              }
+            )
+          })
       }}>
       <OverlayViewF
         position={pos}
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
         <div className="w-[0] h-[0] flex items-center relative justify-center">
+          <div className=" absolute w-[10px] h-[10px] bg-black/40 rounded-full" />
           <div className=" absolute w-[80px] h-[80px] bg-black/10 rounded-full" />
           {MyZoom > 1 ? (
             <div className=" absolute w-[120px] h-[120px] bg-black/10 rounded-full" />
@@ -66,6 +103,11 @@ const MyGoogleMap = function MyGoogleMap({
           <div className="relative flex justify-center items-center">
             <div className="absolute w-16 h-16 rounded-full bg-white p-2">
               <FaHotel className="w-full h-full text-black" />
+              {hotelPos.is_booking ? (
+                <div className="relative w-0 h-0">
+                  <div className="absolute z-10 top-1 right-2">Hi</div>
+                </div>
+              ) : null}
             </div>
           </div>
         </OverlayViewF>
