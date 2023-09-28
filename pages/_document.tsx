faee172fece96a2839aefce7176732fd577644e0
Ahmedmhameed <ahkhateeb02@gmail.com>
2023-09-28 13:09:54 +0300

    Update: add title and Favicon icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,8 @@ export default class MyDocument extends Document {
       <Html lang="en" className="scroll-smooth">
         <Head>
           <link href={font} rel="stylesheet" />
+          <link rel="icon" type="image/x-icon" href="/favicon.png" />
+          <title>Booknap</title>
         </Head>
         <body className="bg-white dark:bg-blue-charcoal font-poppins">
           <Main />
